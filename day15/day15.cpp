b6551eaf2074cc9301562c61683bd576afc65bdf
Olle Mattsson <rymdolle@gmail.com>
2024-02-03 21:30:49 +0100

    Day 15 part 2

--- day15/day15.cpp
+++ day15/day15.cpp
@@ -1,5 +1,7 @@
+#include <algorithm>
 #include <filesystem>
 #include <fstream>
+#include <map>
 #include <sstream>
 #include <string>
 #include <iostream>
@@ -24,14 +26,50 @@ int main(int argc, char *argv[])
   std::string line;
 
   int total = 0;
+  std::map<unsigned char, std::vector<std::pair<std::string, int>>> hashmap;
   while (std::getline(file, line)) {
     std::stringstream ss(line);
     std::string seq;
-    while (std::getline(ss, seq, ','))
+    while (std::getline(ss, seq, ',')) {
       total += hash(seq);
+      if (auto npos = seq.find('='); npos != std::string::npos) {
+        std::string label = seq.substr(0, npos);
+        int focal_length = std::stoi(seq.substr(npos+1));
+        int box = hash(label);
+        bool found = false;
+        for (auto it = hashmap[box].begin(); it != hashmap[box].end(); ++it) {
+          if (it->first == label) {
+            it->second = focal_length;
+            found = true;
+            break;
+          }
+        }
+
+        if (!found) {
+          hashmap[box].emplace_back(label, focal_length);
+        }
+      } else if (auto npos = seq.find('-'); npos != std::string::npos) {
+        std::string label = seq.substr(0, npos);
+        int box = hash(label);
+        for (auto it = hashmap[box].begin(); it != hashmap[box].end(); ++it) {
+          if (it->first == label) {
+            hashmap[box].erase(it);
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  int fpower = 0;
+  for (int box = 0; box < hashmap.size(); ++box) {
+    for (int slot = 0; slot < hashmap[box].size(); ++slot) {
+      fpower += (box + 1) * (slot + 1) * hashmap[box][slot].second;
+    }
   }
 
   std::cout << "part1: " << total << '\n';
+  std::cout << "part2: " << fpower << '\n';
 
   return 0;
 }
