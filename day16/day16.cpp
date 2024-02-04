6ef68e6993d94242e531d88f084fa5410bc05ac4
Olle Mattsson <rymdolle@gmail.com>
2024-02-04 10:50:22 +0100

    Day 16 part 2

--- day16/day16.cpp
+++ day16/day16.cpp
@@ -1,5 +1,6 @@
 #include <filesystem>
 #include <fstream>
+#include <map>
 #include <string>
 #include <iostream>
 #include <vector>
@@ -19,14 +20,14 @@ struct state {
 
 typedef std::vector<std::string> Contraption;
 
-void shine(state s, Contraption& grid, Contraption& tiles)
+void shine(state s, Contraption& grid, std::map<std::pair<int,int>, int>& tiles)
 {
   int x = s.x;
   int y = s.y;
   auto dir = s.dir;
   while (x >= 0 && x < grid[0].size() && y >= 0 && y < grid.size()) {
+    ++tiles[{x,y}];
     if (grid[y][x] == '\\') {
-      tiles[y][x] = '#';
       switch (dir) {
       case direction::east:
         dir = direction::south;
@@ -47,7 +48,6 @@ void shine(state s, Contraption& grid, Contraption& tiles)
       }
 
     } else if (grid[y][x] == '/') {
-      tiles[y][x] = '#';
       switch (dir) {
       case direction::east:
         dir = direction::north;
@@ -70,17 +70,14 @@ void shine(state s, Contraption& grid, Contraption& tiles)
     } else if (grid[y][x] == '-') {
       switch (dir) {
       case direction::east:
-        tiles[y][x] = '#';
         ++x;
         break;
       case direction::west:
-        tiles[y][x] = '#';
         --x;
         break;
       case direction::south:
       case direction::north:
-        if (tiles[y][x] == '.') {
-          tiles[y][x] = '#';
+        if (tiles[{x,y}] == 1) {
           shine({x-1, y, direction::west}, grid, tiles);
           shine({x+1, y, direction::east}, grid, tiles);
         }
@@ -91,24 +88,20 @@ void shine(state s, Contraption& grid, Contraption& tiles)
       switch (dir) {
       case direction::east:
       case direction::west:
-        if (tiles[y][x] == '.') {
-          tiles[y][x] = '#';
+        if (tiles[{x,y}] == 1) {
           shine({x, y-1, direction::north}, grid, tiles);
           shine({x, y+1, direction::south}, grid, tiles);
         }
         return;
       case direction::south:
-        tiles[y][x] = '#';
         ++y;
         break;
       case direction::north:
-        tiles[y][x] = '#';
         --y;
         break;
       }
 
     } else if (grid[y][x] == '.') {
-      tiles[y][x] = '#';
       switch (dir) {
       case direction::east:
         ++x;
@@ -127,6 +120,13 @@ void shine(state s, Contraption& grid, Contraption& tiles)
   }
 }
 
+size_t energize(state s, Contraption c)
+{
+  std::map<std::pair<int,int>, int> t;
+  shine(s, c, t);
+  return t.size();
+}
+
 int main(int argc, char *argv[])
 {
   std::filesystem::path filename("input.txt");
@@ -139,18 +139,22 @@ int main(int argc, char *argv[])
     grid.push_back(line);
   }
 
-  Contraption tiles(grid.size(), std::string(grid[0].size(), '.'));
-  shine({0, 0, direction::east}, grid, tiles);
-
-  int energized = 0;
-  for (auto& row : tiles) {
-    std::cout << row << '\n';
-    for (auto& col : row) {
-      energized += col == '#';
-    }
+  size_t energized_max = 0;
+  for (int start = 0; start < grid.size(); ++start) {
+    int startx = grid[0].size() - 1;
+    int starty = grid[0].size() - 1;
+    size_t max1 = energize({0,      start,  direction::east}, grid);
+    size_t max2 = energize({startx, start,  direction::west}, grid);
+    size_t max3 = energize({start,  0,      direction::south}, grid);
+    size_t max4 = energize({start,  starty, direction::north}, grid);
+    energized_max = std::max(energized_max, max1);
+    energized_max = std::max(energized_max, max2);
+    energized_max = std::max(energized_max, max3);
+    energized_max = std::max(energized_max, max4);
   }
 
-  std::cout << "part1: " << energized << '\n';
+  std::cout << "part1: " << energize({0,0,direction::east}, grid) << '\n';
+  std::cout << "part2: " << energized_max << '\n';
 
   return 0;
 }
